15169d8460c44573b0d345e8dd1d3fe00dd0238c
emijrp <emijrp@25f239b6-92f9-4231-b61e-0ce2aa185c45>
2011-06-10 15:01:08 +0000

    removing database connect method, now all is get using dumps;
    git-svn-id: https://forja.rediris.es/svn/statmediawiki@322 25f239b6-92f9-4231-b61e-0ce2aa185c45

--- branches/np/smwparser.py
+++ branches/np/smwparser.py
@@ -26,7 +26,7 @@ import os
 import hashlib
 import tkMessageBox
 
-#TODO: unificar las funciones parsemywiki y parsemediawiki en la medida de lo posible, ahora mismo hay mucho código repetido
+#TODO:
 #campos adicionales: número de enlaces, categorías, ficheros, plantillas, etc
 #capturar los namespaces del comienzo del XML y meter un campo namespace en la tabla page
 
@@ -136,67 +136,6 @@ def parseMediaWikiXMLDump(dumpfilename, dbfilename):
     # We can also close the cursor if we are done with it
     cursor.close()
     conn.close()
-
-def parseMediaWikiMySQLConnect(mywikicursor, path, filename):
-    import MySQLdb
-    print "FUSIONAR PARA EVITAR COMETER ERRORES EN LAS TABLAS"
-    return
-
-    if not os.path.exists(path):
-        os.makedirs(path)
-    filenamedb = '%s/%s' % (path, filename)
-
-    if os.path.exists(filenamedb):
-        print 'Ya existe un preprocesado para este proyecto'
-
-        if tkMessageBox.askyesno("File exists", "A parsed file exists. Delete and re-parse?"):
-            os.remove(filenamedb)
-        else:
-            return
-
-    conn = sqlite3.connect(filenamedb)
-    cursor = conn.cursor()
-
-    # Create table
-    createDB(cursor)
-
-    limit = 10000
-    c = 0
-    i=0
-    t1=time.time()
-    tt=time.time()
-
-    cursor.execute('SELECT rev_id, page_title, rev_page, rev_user_text, rev_timestamp FROM revision, page WHERE page_id=rev_page')
-    result = cursor.fetchall() #TODO fix hacerlo sin el fetchall, por si el wiki es grande
-    for page_title, rev_page, rev_user_text, rev_timestamp, rev_id in result:
-        timestamp = datetime.datetime(year=int(rev_timestamp[0:4]), month=int(rev_timestamp[5:7]), day=int(rev_timestamp[8:10]), hour=int(rev_timestamp[11:13]), minute=int(rev_timestamp[14:16]), second=int(rev_timestamp[17:19]))
-        md5 = 0#md5 = hashlib.md5(x.text.encode('utf-8')).hexdigest() #el texto esta en old_text de la tabla text
-        ipedit = False#is ip?
-        t = (rev_id, page_title, rev_page, rev_user_text, ipedit, timestamp, md5)
-
-        if not None in t and not '' in t:
-            cursor.execute('INSERT INTO revision VALUES (?,?,?,?,?,?,?)', t)
-            i+=1
-        else:
-            print t
-
-        c += 1
-        if c % limit == 0:
-            print limit/(time.time()-t1), 'ed/s'
-            conn.commit()
-            t1=time.time()
-    conn.commit() #para cuando son menos de limit o el resto
-    print 'Total revisions', c, 'Inserted', i, 'Total time', time.time()-tt, 'seg', (time.time()-tt)/60.0, 'min'
-
-    #tablas auxiliares
-    generateAuxTables(conn=conn, cursor=cursor)
-
-    # We can also close the cursor if we are done with it
-    cursor.close()
-    conn.close()
-
-    print 'OK!'
-    tkMessageBox.showinfo("OK", "Parsing complete")
 
 if __name__ == "__main__":
     filename = sys.argv[1]
