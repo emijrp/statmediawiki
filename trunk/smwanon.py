35675c8207d9b97e0e26126360613ab99acb4dc9
emijrp <emijrp@25f239b6-92f9-4231-b61e-0ce2aa185c45>
2011-07-01 20:34:18 +0000

    fixing anon;
    git-svn-id: https://forja.rediris.es/svn/statmediawiki@330 25f239b6-92f9-4231-b61e-0ce2aa185c45

--- trunk/smwanon.py
+++ trunk/smwanon.py
@@ -44,9 +44,10 @@ def anonimize():
             user_name_ = noise(user_props["user_name_"])
         
         #tiene algun sentido tener distintos md5 (uno para el id, otro para el user_text) de cada usuario?
+        #en la tabla de conversión, dejamos solo el campo user_name_, que es ell que usamos para convertir los dicts de imgs y revs de más abajo
         
-        anonymous_table[user_props["user_id"]] = user_name_
-        anonymous_table[user_props["user_name"]] = user_name_
+        #anonymous_table[user_props["user_id"]] = user_name_
+        #anonymous_table[user_props["user_name"]] = user_name_
         anonymous_table[user_props["user_name_"]] = user_name_
 
         user_props_ = user_props
@@ -60,13 +61,15 @@ def anonimize():
     #anonimize images dict
     for img_name, img_props in smwconfig.images.items():
         img_props_ = img_props
-        img_props_["img_user"] = anonymous_table[img_props_["img_user"]]
-        img_props_["img_user_text"] = anonymous_table[img_props_["img_user_text"]]
+        img_props_["img_user"] = anonymous_table[img_props_["img_user_text_"]]
+        img_props_["img_user_text"] = anonymous_table[img_props_["img_user_text_"]]
+        img_props_["img_user_text_"] = anonymous_table[img_props_["img_user_text_"]]
         smwconfig.images[img_name] = img_props_
 
     #anonimize revisions dict
     for rev_id, rev_props in smwconfig.revisions.items():
         rev_props_ = rev_props
-        rev_props_["rev_user"] = anonymous_table[rev_props_["rev_user"]]
-        rev_props_["rev_user_text"] = anonymous_table[rev_props_["rev_user_text"]]
+        rev_props_["rev_user"] = anonymous_table[rev_props_["rev_user_text_"]]
+        rev_props_["rev_user_text"] = anonymous_table[rev_props_["rev_user_text_"]]
+        rev_props_["rev_user_text_"] = anonymous_table[rev_props_["rev_user_text_"]]
         smwconfig.revisions[rev_id] = rev_props_
